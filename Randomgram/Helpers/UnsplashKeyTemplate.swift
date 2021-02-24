d1609870c8ab2ba25efbaf9dcd4993bf1da1108c
Yulia <tsyrkunova.yu@yandex.ru>
2021-02-24 16:52:00 +0300

    Added template file for api key

---
+++ Randomgram/Helpers/UnsplashKeyTemplate.swift
@@ -0,0 +1,18 @@
+//
+//  UnsplashKeyTemplate.swift
+//  Randomgram
+//
+//  Created by Yulia Tsyrkunova on 24.02.2021.
+//
+
+import Foundation
+
+// Please enter your own API key and uncomment the code :)
+
+/*
+struct UnsplashKey {
+    
+    static let apiKey = ""
+    
+}
+*/
